5ffa6b342f217a9fa23dd58bc4cb145d77e8908b
Haaalatoon <hrhilane@student.1337.ma>
2025-06-30 17:58:22 +0100

    Synthax errors implemented + main modified

--- src/main.c
+++ src/main.c
@@ -58,6 +58,55 @@ void print_expandable_tokens(t_lexer *lexer)
     }
 }
 
+// int main(int ac, char **av, char **envp)
+// {
+//     char *input = NULL;
+//     t_lexer *lexer = NULL;
+    
+//     init_shell(envp);
+//     while (1)
+//     {
+//         input = readline("minishell> ");
+//         if (!input)
+//             break;
+//         if (*input)
+//             add_history(input);
+
+//         lexer = init_lexer(input);
+//         if (!lexer)
+//         {
+//             fprintf(stderr, "Lexer init failed\n");
+//             free(input);
+//             return 1;
+//         }
+//         // Print final EOF
+//         set_context(lexer, '\0');
+//         set_state(lexer);
+//         // Reset offset and resolve tokens
+//         lexer->offset = lexer->input;
+//         resolve_tokens(lexer);
+
+//         // Print resolved tokens
+//         printf("\nResolved Tokens:\n");
+//         t_list *cur = lexer->tokens;
+
+//         while (cur)
+//         {
+//             t_token *tok = (t_token *)cur->data;
+//             printf("Token: %-15s | Type: %-12s | Expandable: %s\n",
+//                 tok->value,
+//                 token_type_to_str(tok->type),
+//                 expendable_to_str(tok->expendable));
+//             cur = cur->next;
+//         }
+//         start(create_pipeline(lexer->tokens));
+//         free_lexer(lexer);
+//         free(input);
+//     }
+//     return 0;
+// }
+
+// Modified main
 int main(int ac, char **av, char **envp)
 {
     char *input = NULL;
@@ -79,16 +128,16 @@ int main(int ac, char **av, char **envp)
             free(input);
             return 1;
         }
-        // Print final EOF
-        set_context(lexer, '\0');
-        set_state(lexer);
-        // Reset offset and resolve tokens
-        lexer->offset = lexer->input;
         resolve_tokens(lexer);
-
+        if (check_syntax_errors(lexer))
+        {
+            free_lexer(lexer);
+            free(input);
+            continue;
+        }
         // Print resolved tokens
         // printf("\nResolved Tokens:\n");
-        t_list *cur = lexer->tokens;
+        // t_list *cur = lexer->tokens;
 
         // while (cur)
         // {
